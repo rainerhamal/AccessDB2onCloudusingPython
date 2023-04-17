995bea86fe2f8ee3f6bf32d6a818926efa45cb2d
Rainer Flores <rainerneno@gmail.com>
2023-04-17 19:31:31 +0800

    fist commit

---
+++ index.py
@@ -0,0 +1,53 @@
+# Task 1: Import the ibm_db Python library
+import ibm_db
+
+# These libraries are pre-installed in SN Labs. If running in another environment please uncomment lines below to install them:
+# !pip install --force-reinstall ibm_db==3.1.0 ibm_db_sa==0.3.3
+# Ensure we don't load_ext with sqlalchemy>=1.4 (incompadible)
+# !pip uninstall sqlalchemy==1.4 -y && pip install sqlalchemy==1.3.24
+# !pip install ipython-sql
+
+# Task 2: Identify the database connection credentials (located in config.py for security added in gitignore)
+# Connecting to dashDB or DB2 database requires the following information:
+#Replace the placeholder values with the actuals for your Db2 Service Credentials
+# dsn_driver = "{IBM DB2 ODBC DRIVER}"
+# dsn_database = "database"            # e.g. "BLUDB"
+# dsn_hostname = "hostname"            # e.g.: "dashdb-txn-sbox-yp-dal09-04.services.dal.bluemix.net"
+# dsn_port = "port"                    # e.g. "50000" 
+# dsn_protocol = "protocol"            # i.e. "TCPIP"
+# dsn_uid = "username"                 # e.g. "abc12345"
+# dsn_pwd = "password"                 # e.g. "7dBZ3wWt9XN6$o0J"
+# dsn_security = "SSL"              #i.e. "SSL"
+from config import dsn_driver, dsn_database, dsn_hostname, dsn_port, dsn_protocol, dsn_uid , dsn_pwd, dsn_security
+
+# Task 3: Create the database connection
+dsn = (
+    "DRIVER={0};"
+    "DATABASE={1};"
+    "HOSTNAME={2};"
+    "PORT={3};"
+    "PROTOCOL={4};"
+    "UID={5};"
+    "PWD={6};"
+    "SECURITY={7};"
+).format(dsn_driver, dsn_database, dsn_hostname, dsn_port, dsn_protocol, dsn_uid , dsn_pwd, dsn_security)
+
+try:
+    conn = ibm_db.connect(dsn, "", "")
+    print("Connected to database: ", dsn_database, "as user: ", dsn_uid, "on host: ", dsn_hostname)
+except:
+    print("Unable to connect: ", ibm_db.conn_errormsg())
+
+# Task 4: Create a table in the database
+# Let's first drop the table Instructor in case it exists from a previous attempt
+dropQuery = "drop table INSTRUCTOR"
+
+# Now execute the drop statement
+drpStmt = ibm_db.exec_immediate(conn, dropQuery)
+# Dont worry if you get this error:
+# If you see an exception/error similar to the following, indicating that INSTRUCTOR is an undefined name, that's okay. It just implies that the INSTRUCTOR table does not exist in the table - which would be the case if you had not created it previously.
+# Exception: [IBM][CLI Driver][DB2/LINUXX8664] SQL0204N "ABC12345.INSTRUCTOR" is an undefined name. SQLSTATE=42704 SQLCODE=-204
+
+# Construct the Create Table DDl statement -replace the... with trest of the statement
+
+
